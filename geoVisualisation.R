b260db2b464a701b9b6ac9d03bb5fccb9c49cac4
nahku <nsaidy@t-online.de>
2020-01-20 12:48:09 +0100

    progress on markdown

--- geoVisualisation.R
+++ geoVisualisation.R
@@ -5,8 +5,15 @@ library(gridExtra)
 library(ggmap)
 library(osmdata)
 library(readxl)
-library(DataCombine)
-germany_map <- get_map(getbb("Deutschland", base_url = "https://nominatim.openstreetmap.org", featuretype = "country"),maptype = "watercolor")
+
+
+#Shape Map
+map <- st_read("data/2500_NUTS1.shp", stringsAsFactors=FALSE)
+map = st_transform(map,3857)
+ggplot(map) + geom_sf()
+
+#Real Map
+germany_map <- get_map(getbb("Deutschland", base_url = "https://nominatim.openstreetmap.org", featuretype = "country"),maptype = "toner-background")
 ggmap(germany_map)
 ggmap_bbox <- function(map) {
   if (!inherits(map, "ggmap")) stop("map must be a ggmap object")
@@ -27,68 +34,59 @@ ggmap_bbox <- function(map) {
   map
 }
 
-# Use the function:
 germany_map <- ggmap_bbox(germany_map)
 
-ggmap(germany_map)
-my_data <- read_excel("binnenwanderung.xlsx")
+
+#Import Data
+binnenwanderung_data <- read_excel("binnenwanderung.xlsx")
 states <- map$NUTS_NAME
-#votes_per_state2 <- data.frame(NUTS_NAME = c(1:16), Data = c(1:16))
-votes_per_state2 <- data.frame()
-votes_per_state_mean <- data.frame(NUTS_NAME = c(1:16), Mean_Migration = c(1:16))
+movement_per_state <- data.frame(matrix(ncol = 16, nrow = 16))
+x <- c("NUTS_NAME",sprintf("%s",2003:2017))
+colnames(movement_per_state) <- x
+movement_per_state_mean <- data.frame(NUTS_NAME = c(1:16), Mean_Migration = c(1:16))
 
 for(i in 1:length(states)){
-  #votes_per_state2$NUTS_NAME[i] = states[i]
-  tmp <- which(my_data == states[i], arr.ind = TRUE)[1]
-  data = my_data[tmp,]
-  data2 = data[-2:-3]
-  
-  votes_per_state2 <- rbind(votes_per_state2,data2)
-  votes_per_state_mean$NUTS_NAME[i] = states[i]
-  votes_per_state_mean$Mean_Migration[i] = mean(as.numeric(as.character(data[4:18])))
+  index <- which(binnenwanderung_data == states[i], arr.ind = TRUE)[1]+2
+  row_data = binnenwanderung_data[index,-1:-3]
+  data_frame_row = c(c(states[i]), row_data)
+  names(data_frame_row) <- x
+  movement_per_state[i,] = data_frame_row
+  movement_per_state_mean$NUTS_NAME[i] = states[i]
+  movement_per_state_mean$Mean_Migration[i] = mean(as.numeric(as.character(row_data)))
 }
-names(votes_per_state2)[names(votes_per_state2) == 'Bundesland'] <- 'NUTS_NAME'
 
-ost = c("Brandenburg","Mecklenburg-Vorpommern","Sachsen","Sachsen-Anhalt","Thüringen")
-test = votes_per_state2[ost,]
-ost_votes = votes_per_state2[votes_per_state2$Bundesland %in% ost,]
-ost_votes = ost_votes[1:5,]
-a = colSums(ost_votes[,-1])
+new_states = c("Brandenburg","Mecklenburg-Vorpommern","Sachsen","Sachsen-Anhalt","Thüringen")
+new_states_movement = movement_per_state[movement_per_state$NUTS_NAME %in% new_states,]
+new_states_movement = new_states_movement[1:5,]
+data_of_new_states = colSums(new_states_movement[,-1])
 
 `%notin%` <- Negate(`%in%`)
 
-west_votes = votes_per_state2[votes_per_state2$Bundesland %notin% ost,]
-west_votes = west_votes[1:11,]
-b = colSums(west_votes[,-1])
-#Plot new and old States
-plot(b,type = "o",col = "blue",xaxt="n",ylim=c(-50000,50000),xlab="Jahr",ylab="Saldo",main="Saldo der Binnenwanderung zwischen neuen und alten Bundesländern",sub="(Berlin wird hier zu den alten Bundesländern gezählt)")
-lines(a,type = "o",col = "red")
+old_states_movement = movement_per_state[movement_per_state$NUTS_NAME %notin% new_states,]
+old_states_movement = old_states_movement[1:11,]
+data_of_old_states = colSums(old_states_movement[,-1])
+
+#Plot Movement in new and old States
+plot(data_of_old_states,type = "o",col = "blue",xaxt="n",ylim=c(-55000,55000),xlab="Jahr",ylab="Saldo",main="Saldo der Binnenwanderung zwischen neuen und alten Bundesländern",sub="(Berlin wird hier zu den alten Bundesländern gezählt)")
+lines(data_of_new_states,type = "o",col = "red")
 axis(1,at=1:15,lab=c(2003:2017))
 grid(nx = NULL, ny = NULL, col = "black", lty = "dotted")
 legend(10,-35000,legend=c("Alte Bundesländer","Neue Bundesländer"),col =c("blue","red"),lty=1,cex=1)
 
-a= which(my_data == "Bayern", arr.ind = TRUE)
-#b = a[1]+2
-#bayern = my_data[b,]
-#bayern$ 
-map <- st_read("data/2500_NUTS1.shp", stringsAsFactors=FALSE)
-map = st_transform(map,3857)
-str(map)
-ggplot(map) + geom_sf()
+merged_movement_mean <- left_join(map,movement_per_state_mean,by="NUTS_NAME")
+#binnenwanderung <- left_join(map,movement_per_state,by="NUTS_NAME")
+#ggplot(binnenwanderung) + geom_sf(aes(fill= binnenwanderung$'2013'))+
+# scale_fill_gradient2(low= "#00008B", mid="white",high = "#FF3300")
+
+ggmap(germany_map) + coord_sf(crs=st_crs(3857)) + geom_sf(data=binnenwanderung, aes(fill= merged_movement_mean$Mean_Migration), inherit.aes = FALSE,alpha=0.5)+
+  scale_fill_gradient2(low= "#00008B", mid="white",high = "#FF3300") 
+
+
+
 #mydata = read.csv("Studium/Semester 5/Datenanalyse mit R/Abgabe/Stimmenanzahl2.csv", sep=";")
 #mydata <- read.csv(file="Studium/Semester 5/Datenanalyse mit R/Abgabe/Stimmenanzahl2.csv", sep=";", colClasses=c(NA, "NULL", "NULL", "NULL"))
 #stimmen <- read.csv(file="Studium/Semester 5/Datenanalyse mit R/Abgabe/Stimmenanzahl2.csv", sep=";", colClasses=c("NULL", NA, "NULL", "NULL"))
 stimmen <- read.csv(file="btw17_kerg.csv", sep=";")
-
-#umlaute <- function(variable) { 
-#  variable <- gsub("ü","?",variable) 
-#  variable <- gsub("ß","?",variable) 
-#  variable <- gsub("ör","?",variable) 
-#  variable <- gsub("ä","?",variable) 
-#  return(variable) }
-#stimmen$X <- umlaute(stimmen$X)
-
-
 
 extracted_votes <- data.frame(Votes = stimmen$X.16, VotesAfD = stimmen$X.44)
 
@@ -109,28 +107,19 @@ einkommen <- read.csv(file="Stimmenanzahl2.csv", sep=";", colClasses=c("NULL", "
 arbeitslos <- data.frame(NUTS_NAME = map$NUTS_NAME, ARBEITSLOSENQUOTE = arbeitslos)
 einkommen <- data.frame(NUTS_NAME = map$NUTS_NAME, NETTOEINKOMMEN = einkommen)
 
-#merged_stimmen <- left_join(map,votes_per_state,by="NUTS_NAME")
-#plot_stimmen <- ggplot(merged_stimmen) + geom_sf(aes(fill= Votes_AfD))+
-#  scale_fill_gradient(low= "#E0E0E0",mid="#FFFFFF", high = "#FF3300")
+merged_stimmen <- left_join(map,votes_per_state,by="NUTS_NAME")
+plot_stimmen <- ggplot(merged_stimmen) + geom_sf(aes(fill= Votes_AfD))+
+ scale_fill_gradient(low= "#E0E0E0",high = "#FF3300")
 
-#binnenwanderung <- left_join(map,votes_per_state_mean,by="NUTS_NAME")
-binnenwanderung <- left_join(map,votes_per_state2,by="NUTS_NAME")
-#ggplot(binnenwanderung) + geom_sf(aes(fill= binnenwanderung$'2013'))+
- # scale_fill_gradient2(low= "#00008B", mid="white",high = "#FF3300")
-
-ggmap(germany_map) + coord_sf(crs=st_crs(3857)) + geom_sf(data=binnenwanderung, aes(fill= binnenwanderung$'2013'), inherit.aes = FALSE,alpha=0.5)+
-  scale_fill_gradient2(low= "#00008B", mid="white",high = "#FF3300") 
-
-plot(st_transform(map,crs = 3857)[1], bgMap = germany_map)
+#plot(st_transform(map,crs = 3857)[1], bgMap = germany_map)
 merged_arbeitslos <- left_join(map,arbeitslos,by="NUTS_NAME")
 plot_arbeitslos <- ggplot(merged_arbeitslos) + geom_sf(aes(fill= Arbeitslosenquote))+
   scale_fill_gradient(low= "#E0E0E0", high = "#000080")
 
-
-grid.arrange(plot_stimmen, plot_arbeitslos, ncol=2)
+grid.arrange(plot_stimmen, plot_einkommen, ncol=2)
 
 merged_einkommen <- left_join(map,einkommen,by="NUTS_NAME")
-ggplot(merged_einkommen) + geom_sf(aes(fill= Nettoeinkommen))+
+plot_einkommen <- ggplot(merged_einkommen) + geom_sf(aes(fill= Nettoeinkommen))+
   scale_fill_gradient(low= "#E0E0E0", high = "#FF3300")
 
 x <- votes_per_state[,2]
@@ -140,4 +129,3 @@ cor.test(x,y,method="pearson")
 #https://de.statista.com/statistik/daten/studie/5758/umfrage/verfuegbares-nettoeinkommen-nach-bundeslaendern/
 #https://de.statista.com/statistik/daten/studie/36651/umfrage/arbeitslosenquote-in-deutschland-nach-bundeslaendern/
 #https://www.bundeswahlleiter.de/bundestagswahlen/2017/ergebnisse/bund-99/land-12.html
-
